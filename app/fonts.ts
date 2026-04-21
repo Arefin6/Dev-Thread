06be51472593ecf639055836465cf6221f96be20
arefin6 <arefinhossain3@gmail.com>
2026-04-21 12:55:51 +0600

    Metadata fonts and assets complete

---
+++ app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Space_Grotesk } from "next/font/google";
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+export const space_Grotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { space_Grotesk } from "./fonts";
+
 export default function Home() {
   return (
-    <div className="text-2xl text-amber-200">
+    <div>
       <h2>Welcome to Dev Thread</h2>
+      <h2 className={space_Grotesk.className}>
+        Welcome to Dev Thread Another Font
+      </h2>
     </div>
   );
 }
