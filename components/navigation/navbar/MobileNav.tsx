65128ae735d1dbff67d68aed9bc5af82d58733c0
arefin6 <arefinhossain3@gmail.com>
2026-06-13 16:00:24 +0600

    mobile nav complete

--- components/navigation/navbar/MobileNav.tsx
+++ components/navigation/navbar/MobileNav.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+import { auth, signOut } from "@/app/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import ROUTES from "@/constants/routes";
+import { LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import NavLinks from "./NavLinks";
 
-const MobileNav = () => {
-  return <div>MobileNav</div>;
+const MobileNav = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Image
+          src="/icons/hamburger.svg"
+          alt="Hamburger Menu Icon"
+          width={36}
+          height={36}
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <SheetTitle className="hidden">Navigation</SheetTitle>
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Logo"
+          />
+
+          <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
+            Dev<span className="text-primary-500">Thread</span>
+          </p>
+        </Link>
+
+        <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <SheetClose>
+            <section className="flex h-full flex-col gap-6 pt-16">
+              <NavLinks isMobileNav />
+            </section>
+          </SheetClose>
+
+          <div className="flex flex-col gap-3">
+            {userId ? (
+              <SheetClose>
+                <form
+                  action={async () => {
+                    "use server";
+
+                    await signOut();
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    className="base-medium w-fit !bg-transparent px-4 py-3"
+                  >
+                    <LogOut className="size-5 text-black dark:text-white" />
+                    <span className="text-dark300_light900">Logout</span>
+                  </Button>
+                </form>
+              </SheetClose>
+            ) : (
+              <>
+                <SheetClose>
+                  <Link href={ROUTES.SIGNIN}>
+                    <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none cursor-pointer">
+                      <span className="primary-text-gradient">Log In</span>
+                    </Button>
+                  </Link>
+                </SheetClose>
+
+                <SheetClose>
+                  <Link href={ROUTES.SIGNUP}>
+                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none cursor-pointer">
+                      Sign Up
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </>
+            )}
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
