947d5599663d6fa419e5c373d62095fc5915ba81
arefin6 <arefinhossain3@gmail.com>
2026-06-16 15:32:29 +0600

    implemented basic layout and static right side bar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,5 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGNIN: "/sign-in",
   SIGNUP: "/sign-up",
+  QUESTION: (id: string) => `/question/${id}`,
+  TAG: (id: string) => `/tags/${id}`,
 };
 export default ROUTES;
