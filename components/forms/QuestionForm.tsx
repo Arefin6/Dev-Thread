8e056bea54784aee4ae6d99977691b8d27e3a4e0
arefin6 <arefinhossain3@gmail.com>
2026-06-28 22:33:14 +0600

    Ask Question form ui done

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,14 +1,28 @@
-"use clint";
-
+"use client";
 import { AskQuestionSchema } from "@/lib/validations";
-import { Form } from "@base-ui/react";
-
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import z from "zod";
-import { Field } from "../ui/field";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "../ui/field";
+import { Input } from "../ui/input";
+import TagCard from "../Cards/TagCard";
+import { Button } from "../ui/button";
+import { useTransition } from "react";
+import { RefreshCcw } from "lucide-react";
 
-const QuestionForm = () => {
+interface Params {
+  question?: Question;
+  isEdit?: boolean;
+}
+
+const QuestionForm = ({ question, isEdit = false }: Params) => {
+  const [isPending, startTransition] = useTransition();
   const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -20,8 +34,113 @@ const QuestionForm = () => {
   const handleCreateQuestion = () => {};
 
   return (
-    <form>
-      <h2>Form</h2>
+    <form
+      className="flex w-full flex-col gap-10"
+      onSubmit={form.handleSubmit(handleCreateQuestion)}
+    >
+      <FieldGroup>
+        <Controller
+          name="title"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field
+              className="flex w-full flex-col"
+              data-invalid={fieldState.invalid}
+            >
+              <FieldLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FieldLabel>
+              <Input
+                {...field}
+                className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+              />
+              <FieldDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person.
+              </FieldDescription>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="content"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field
+              className="flex w-full flex-col"
+              data-invalid={fieldState.invalid}
+            >
+              <FieldLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FieldLabel>
+              Editor
+              <FieldDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you&apos;ve put in the
+                title.
+              </FieldDescription>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="tags"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field
+              className="flex w-full flex-col"
+              data-invalid={fieldState.invalid}
+            >
+              <FieldLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FieldLabel>
+              <div>
+                <Input
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
+                  placeholder="Add tags..."
+                  //onKeyDown={(e) => handleInputKeyDown(e, field)}
+                />
+                {field.value.length > 0 && (
+                  <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                    {field?.value?.map((tag: string) => (
+                      <TagCard
+                        key={tag}
+                        _id={tag}
+                        name={tag}
+                        compact
+                        //remove
+                        //isButton
+                        //handleRemove={() => handleTagRemove(tag, field)}
+                      />
+                    ))}
+                  </div>
+                )}
+              </div>
+              <FieldDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 5 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FieldDescription>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+      </FieldGroup>
+      <div className="mt-16 flex justify-end">
+        <Button
+          type="submit"
+          disabled={isPending}
+          className="primary-gradient w-fit !text-light-900 cursor-pointer"
+        >
+          {isPending ? (
+            <>
+              <RefreshCcw className="mr-2 size-4 animate-spin" />
+              <span>Submitting</span>
+            </>
+          ) : (
+            <>{isEdit ? "Edit" : "Ask a Question"}</>
+          )}
+        </Button>
+      </div>
     </form>
   );
 };
